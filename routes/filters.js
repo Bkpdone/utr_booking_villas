6c82546b7934351a7d3ae47ec95060779eec07ce
Bkpdone <pharatebhavesh@gmail.com>
2024-02-07 11:56:57 +0530

    update filter

--- routes/filters.js
+++ routes/filters.js
@@ -28,8 +28,30 @@ router.get('/auto-filter/:substr', async (req, res) => {
         // const suggestions = await Property.find({ locationField: wordRegex }).limit(10);
         //const suggestions = await Property.find({ loctionField: { $regex: substrData, $options: 'i' } }).limit(10);
 
+
+
+        function correctSpelling(userInput) {
+            userInput = userInput.toLowerCase(); 
+            const inputWords = userInput.split(' ');
+        
+            // Correct each word in the input
+            const correctedWords = inputWords.map(word => {
+                const isCorrect = dictionary.check(word);
+                return isCorrect ? word : dictionary.suggest(word)[0];
+            });
+        
+            return correctedWords.join(' ');
+        }
+        
+        // Example usage
+        const userInput = "favouriite colur";
+        const correctedInput = 'ok'//correctSpelling(userInput);
+        console.log(correctedInput);
+
+
         const suggestions = await Property.find({ loctionField: { $regex: new RegExp(`\\b${substrData}`, 'i') } }).limit(10);
         // const locationValues = suggestions.map((property) => property.loctionField);
+        
 
         const locationValues = suggestions.map((property) => ({
             locationField: property.loctionField,
@@ -37,7 +59,10 @@ router.get('/auto-filter/:substr', async (req, res) => {
             country: property.location.country,
             city: property.location.city
         }))
-        console.log('Balaji Bala kare mera', suggestions);
+
+
+
+       // console.log('Balaji Bala kare mera', suggestions);
         res.status(200).json({
             success: true,
             data: locationValues
@@ -56,7 +81,7 @@ router.get('/auto-filter/:substr', async (req, res) => {
 router.get('/filter-1', async (req, res) => {
 
     try {
-        const res = await Property
+        const res = await Property.find()
         
     }
     catch (err) {
